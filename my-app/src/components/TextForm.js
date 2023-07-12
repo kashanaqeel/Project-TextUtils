fbb1d737313f37cee60adf35307a6fbd8c68e1b2
Kashan Aqeel <kashanaqeel74@gmail.com>
2023-07-12 15:19:56 +0500

    text-area-added

---
+++ my-app/src/components/TextForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function TextForm() {
+  return (
+    <div>
+  <div className="mb-3">
+    <label for="box" className="form-label"></label>
+    <textarea className="form-control" id="box" rows="3"></textarea>
+  </div>
+  </div>
+  )
+}
