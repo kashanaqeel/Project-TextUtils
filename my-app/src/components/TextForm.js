1491e3023fa4ed85f10efd50ab8b6ed21be0bb0f
Kashan Aqeel <kashanaqeel74@gmail.com>
2023-07-12 15:54:41 +0500

    UpperCase-fuctionality-added
    using useState hook

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export default function TextForm(props) {
-  return (
+    const handleUpClick=()=>{
+        let newtext=text.toUpperCase();
+        setText(newtext)
+    }
+    
+    const handleOnChange=(event)=>{
+        setText(event.target.value)
+    }
+    const [text, setText] = useState('Enter text here');
+    return (
     <div>
         <h1>{props.heading}</h1>
   <div className="mb-3">
-    <textarea className="form-control" id="box" rows="8"></textarea>
+    <textarea className="form-control" id="box" value={text} onChange={handleOnChange} rows="8"></textarea>
   </div>
-  <button className="btn btn-primary">Convert to UpperCase</button>
+  <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
   </div>
   )
 }
