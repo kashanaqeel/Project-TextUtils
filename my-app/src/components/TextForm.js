f78958bc273d891a0f1b74204fcd86c3d10106ee
Kashan Aqeel <kashanaqeel74@gmail.com>
2023-07-12 16:10:02 +0500

    Lower-Case-Functionality-Added

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -5,18 +5,30 @@ export default function TextForm(props) {
         let newtext=text.toUpperCase();
         setText(newtext)
     }
-    
+    const handleLowClick=()=>{
+        let newtext=text.toLowerCase();
+        setText(newtext)
+    }
     const handleOnChange=(event)=>{
         setText(event.target.value)
     }
     const [text, setText] = useState('Enter text here');
     return (
-    <div>
+        <>
+    <div className='container'>
         <h1>{props.heading}</h1>
   <div className="mb-3">
     <textarea className="form-control" id="box" value={text} onChange={handleOnChange} rows="8"></textarea>
   </div>
-  <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+  <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+  <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
   </div>
+  <div className="container my-2"><h1>Your text summary</h1>
+  <p>{text.split(" ").length} words and {text.length} characters</p>
+  <p>{0.008 * text.split(" ").length} minutes read</p>
+  <h2>Preview</h2>
+  <p>{text}</p>
+  </div>
+  </>
   )
 }
