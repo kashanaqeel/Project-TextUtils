25b69a887752e0b989663dbc5fcb1b3ede01907a
Kashan Aqeel <kashanaqeel74@gmail.com>
2023-07-12 15:22:22 +0500

    text-area-props-added

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export default function TextForm() {
+export default function TextForm(props) {
   return (
     <div>
+        <h1>{props.heading}</h1>
   <div className="mb-3">
-    <label for="box" className="form-label"></label>
+    <label for="box" className="form-label">example textarea</label>
     <textarea className="form-control" id="box" rows="3"></textarea>
   </div>
   </div>
